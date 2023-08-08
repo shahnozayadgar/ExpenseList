684a254d4a4dd4c1edf41b08f340b7c93911dc19
shahnozayadgar <yadgarova@kaist.ac.kr>
2023-08-08 13:21:58 +0300

    sixth

---
+++ components /Expenses.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import './Expenses.css';
+import ExpensesFilter from "./NewExpense/ExpenseFilter/ExpensesFilter";
+import Card from "./UI/Card";
+import ExpensesList from "./ExpensesList/ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    }); 
+
+    return (
+    <div>
+        <Card className="expenses">
+        <ExpensesFilter 
+        selected={filteredYear} 
+        onChangeFilter={filterChangeHandler} 
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+        </Card>
+    </div>
+    );
+}
+
+export default Expenses;
